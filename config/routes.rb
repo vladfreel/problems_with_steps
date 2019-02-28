29ecafa318a92c8da3334ffdf300f93dfc13736f
vladfreel <vladfreelmaster@gmail.com>
2019-02-28 09:58:35 +0200

    add haml

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'problems#index'
   resources :steps
   resources :problems
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
